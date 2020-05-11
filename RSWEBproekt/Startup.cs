c20007518323250da1b8d5130aa1dd8f718aeb62
Zaneta <zanetatrenceva1@gmail.com>
2020-05-11 20:57:43 +0200

    ApiControllers

--- RSWEBproekt/Startup.cs
+++ RSWEBproekt/Startup.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -30,7 +24,7 @@ namespace RSWEBproekt
             services.AddControllersWithViews();
             services.AddDbContext<RSWEBproektContext>(options =>
         options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            }
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -49,7 +43,6 @@ namespace RSWEBproekt
             app.UseStaticFiles();
 
             app.UseRouting();
-            //app.UseSession();
 
             app.UseAuthorization();
 
